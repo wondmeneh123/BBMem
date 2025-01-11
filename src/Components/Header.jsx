018f7418f9f80efba1e0a6c988eaaee860ab9d9a
wondmeneh123 <wondmenhfekadu@gmail.com>
2025-01-11 06:28:29 -0800

    done aded

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="bg-white text-gray-800 py-3 shadow-md w-full fixed">
-      <h1 className="text-2xl sm:text-3xl font-bold text-center">
+    <div className="bg-white text-gray-800 py-3 shadow-md w-full fixed flex justify-center flex-col">
+      <Link to="/" className="text-2xl sm:text-3xl font-bold text-center">
         💖 Mi Amor 💖
-      </h1>
+      </Link>
       <p className="text-center italic mt-1 sm:mt-2 text-gray-600 text-sm sm:text-base">
         "You're the sunshine in my day and the stars in my night." 🌟
       </p>
